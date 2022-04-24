b638bdad6ec01a24067069fa4314c26b91adf9a9
Jason Hammock <jhammock@protonmail.com>
2022-04-23 20:43:18 -0600

    remove unused code

--- cmd/web/book/book.go
+++
@@ -1,7 +0,0 @@
-package book
-
-type Book struct {
-	Id       int64  `json:"id"`
-	Title    string `json:"title"`
-	Location string `json:"location"`
-}
--- cmd/web/user/user.go
+++
@@ -1,8 +0,0 @@
-package user
-
-type User struct {
-	Id          int64  `json:"id"`
-	Email       string `json:"user_name"`
-	Password    string `json:"-"`
-	DisplayName string `json:"display_name"`
-}
